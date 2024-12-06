29139bdd70d7b24dde52d7539b34ae5df1d7d138
原田愛子 <haradaaiko@MacBook-Pro-2.local>
2024-12-06 16:14:22 +0900

    Update index.js with new database connection

--- index.js
+++ index.js
@@ -17,13 +17,24 @@ const port = process.env.PORT || 3000;
 const saltRounds = 10;
 env.config();
 
-const db = new pg.Client({
+/*const db = new pg.Client({
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT,
+  ssl: {
+    rejectUnauthorized: false,  // RenderのSSL接続に必要
+  },
+});*/
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
 db.connect();
 
 // ESモジュール用の__dirnameの設定
